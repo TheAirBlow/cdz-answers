64fb4cb3b74a05edc8d93c43cf898214ad52f555
TheAirBlow <pilexet.danila@gmail.com>
2022-02-25 23:30:29 +0500

    Initial commit

---
+++ TheAirBlow.Solver.WebServer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TheAirBlow.Solver.WebServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
